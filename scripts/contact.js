95685706cf5a647615b83b8d9652a29f1c06a222
KiefferCa <campbellkieffer@yahoo.com>
2025-09-18 22:17:02 -0500

    Java

---
+++ scripts/contact.js
@@ -0,0 +1,17 @@
+console.log("loaded contact.js");
+//alert("contact page script loaded")
+
+function validateForm() {
+    //Get form elements
+    const email = document.getElementById("email_input").value;
+    const password = document.getElementById("password_input").value;
+
+    console.log("Email:", email)
+    console.log("Password:", password)
+
+    //Simple valdation
+    //if(email === ** || password === **){
+    //alert("Please fill in all fields.");
+    //return false;
+    //}
+}
